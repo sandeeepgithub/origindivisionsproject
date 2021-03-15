11c686cb0ed7dc44f5a36a82fbfa85ebeb5d91a4
Sandeep Modi <sandeeep.github@gmail.com>
2021-03-15 20:08:57 +0530

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import '../src/css/animate.css/animate.min.css'
-
-ReactDOM.render(<App />, document.getElementById('root'));
